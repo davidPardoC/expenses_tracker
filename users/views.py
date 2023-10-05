b9a3d2fbf1fedf70d27d44d20c7cd9d3b1387698
davidPardoC <pardodavid10@gmail.com>
2023-10-04 23:06:25 -0500

    Add login views and serializers

--- users/views.py
+++ users/views.py
@@ -1,16 +1,23 @@
-from django.shortcuts import render
-from rest_framework import routers, serializers, viewsets
-from django.contrib.auth.models import User
-
-# Create your views here.
-
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from users.models import User
+from rest_framework.response import Response
+from users.serializers import UserLoginSerializer, UserModelSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class UserViewSet(viewsets.GenericViewSet):
+
+    queryset = User.objects.filter(is_active=True)
+    serializer_class = UserModelSerializer
+
+    @action(detail=False, methods=['post'])
+    def login(self, request):
+        """User login"""
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user, token = serializer.save()
+        data = {
+            'user': UserModelSerializer(user).data,
+            'access_token': token
+        }
+        return Response(data, status=status.HTTP_201_CREATED)
