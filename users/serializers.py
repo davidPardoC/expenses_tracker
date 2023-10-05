f027ac6d94d905a49857c1dcd26d904a7b8bf6de
davidPardoC <pardodavid10@gmail.com>
2023-10-05 17:28:37 -0500

    Add user detail view

--- users/serializers.py
+++ users/serializers.py
@@ -10,7 +10,8 @@ class UserModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
 
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ('username', 'first_name', 'last_name',
+                  'email', 'monthly_budget')
 
 
 class UserLoginSerializer(serializers.Serializer):
