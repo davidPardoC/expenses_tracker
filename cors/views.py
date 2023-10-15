27e7fb4a42cca13db1a5f3537886442eb0ebfa4e
davidPardoC <pardodavid10@gmail.com>
2023-10-15 00:14:18 -0500

    Remove unnecesary routes

--- cors/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def index(request):
-    return render(request, "dist/index.html")
-
-def logo(request):
-    return render(request, "dist/logo.svg")
--- expenses_tracker/settings.py
+++ expenses_tracker/settings.py
@@ -63,7 +63,6 @@ ROOT_URLCONF = 'expenses_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'react_app')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,7 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/assets/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "react_app", "dist", "assets"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
